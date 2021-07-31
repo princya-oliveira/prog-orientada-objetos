51fb676a2f4b71dc7022c27f07239eef349eddea
Princya Oliveira <princya.oliveira@outlook.com>
2021-07-30 22:07:20 -0300

    (30/07) classes e objetos

--- Carro.java
+++ Carro.java
@@ -9,4 +9,24 @@ public class Carro {
     boolean motor; // true (ligado) - false (desligado)
     int ano; 
     String modelo; // String é uma classe pronta - conjunto de caracteres 
+
+    // método construtor
+    public Carro(){
+        
+    }
+
+    // método construtor
+    public Carro(String modelo, int ano, boolean motor, float velocidade) {
+        // a variável do objeto = variável do usuário
+        this.modelo = modelo;
+        this.ano = ano;
+        this.motor = motor;
+        this.velocidade = velocidade;
+    }
+
+    // um método ou operação
+    public void mostra(){
+        // qual palavra representa o objeto que se chama método
+        System.out.println("Modelo: " + this.modelo + " Ano: " + this.ano + " Vel: " + this.velocidade);
+    }
 }
--- TestaCarro.java
+++ TestaCarro.java
@@ -14,13 +14,9 @@ public class TestaCarro {
         obj1.motor = true;
         obj1.velocidade = 30;
 
-        Carro obj2 = new Carro();
-        obj2.modelo = "Celta";
-        obj2.ano = 2016;
-        obj2.motor = true;
-        obj2.velocidade = 50;
-
-        System.out.println("Obj1: Modelo: " + obj1.modelo + " Ano: " + obj1.ano + " Vel: " + obj1.velocidade);
-        System.out.println("Obj2: Modelo: " + obj2.modelo + " Ano: " + obj2.ano + " Vel: " + obj2.velocidade);
+        Carro obj2 = new Carro("Celta", 2016, true, 50);
+        
+        obj1.mostra();
+        obj2.mostra();
     }
 }
